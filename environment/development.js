28f5a32efc5fa2b68d8d422e6ff2e4d84fff4768
thierno <madiouda130@gmail.com>
2025-07-20 22:05:24 +0000

    fix typo

--- environment/development.js
+++ environment/development.js
@@ -3,6 +3,6 @@ const path = require("path");
 module.exports = {
   dbUrl: "mongodb://alex:qwe@localhost/twitter?authSource=admin",
   // "mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true",
-  //   cert: path.join(__dirname, "../ssl/localhost.crt"),
-  //   key: path.join(__dirname, "../ssl/localhost.key"),
+    // cert: "/etc/letsencrypt/live/devops-formation.site/privkey.pem",
+    // key: "/etc/letsencrypt/live/devops-formation.site/fullchain.pem",
 };
